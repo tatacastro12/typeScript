5b992bf1ff097d1a899d847ac6f94bc2e4a0d171
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-04-04 19:34:39 -0700

    practica

--- src/08-herencia.ts
+++ src/08-herencia.ts
@@ -1,15 +1,42 @@
 export class animal {
-  constructor(public name: string) {}
+  constructor(public name: string) { }
 
-  move(){
+  move() {
     console.log('Moving along');
 
   }
-  greeting(){
+  greeting() {
     return `hello, Iam ${this.name}`
   }
 }
- const fifi = new animal('fifi');
- fifi.move();
- console.
- log(fifi.greeting());
+
+export class Dog extends animal {
+  constructor(
+     name: string,
+    public owner: string
+  ) {
+    super(name)
+  }
+
+  woof(times: number): void {
+    for (let index = 0; index < times; index++) {
+      console.log('woof!');
+
+
+    }
+  }
+}
+
+const fifi = new animal('fifi');
+fifi.move();
+console.
+  log(fifi.greeting());
+
+const cheis = new Dog('cheis', "jhon");
+cheis.move();
+console.
+  log(cheis.greeting());
+cheis.woof(5);
+console.log(cheis.owner);
+
+
